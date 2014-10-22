d7124935d824bfded64076ba81580a38afa7a37f
fengkaicnic <fengkai@cnic.cn>
2014-10-22 15:13:15 +0800

    traffic api class

--- traffic/compute/__init__.py
+++ traffic/compute/__init__.py
@@ -3,4 +3,4 @@ import traffic.flags
 import traffic.openstack.common.importutils
 
 API = traffic.openstack.common.importutils.import_class(
-              traffic.flags.FLAGS.compute_api_class)
+              traffic.flags.FLAGS.traffic_api_class)
