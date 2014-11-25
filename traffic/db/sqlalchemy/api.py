e2df74bcc7bf833dc61871c885e6efefa32037e1
fengkaicnic <fengkai@cnic.cn>
2014-11-25 15:19:49 +0800

    chanage meth

--- traffic/db/sqlalchemy/api.py
+++ traffic/db/sqlalchemy/api.py
@@ -796,8 +796,14 @@ def tqdisc_get_all_bk(context):
     
     return result
 
+def get_host_by_instance_id(context, instance_id):
+    session = get_session()
+    result = session.execute('select * form tqdisc where instanceid=\"'+instance_id+'\"')
+
+    return result
+
 @require_context
-def get_host_by_instance_id(context, instance_id, session=None):
+def get_host_by_instance_idbk(context, instance_id, session=None):
     result = model_query(context, models.Instance, project_only=True).\
                  filter_by(instanceid=instance_id).\
                  first()
