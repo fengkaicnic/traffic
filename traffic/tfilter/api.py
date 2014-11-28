112b9ec972f2fbd9348de5167e9860f1bbbc8aaf
fengkaicnic <fengkai@cnic.cn>
2014-11-28 10:13:37 +0800

    change eth0

--- traffic/tfilter/api.py
+++ traffic/tfilter/api.py
@@ -33,7 +33,7 @@ class API(base.Base):
     def delete(self, context, instanceid):       
         tfilter = self.db.tfilter_get_by_instance(context, instanceid)
         handle_r = '800::' + str(tfilter[6])
-        cmd = ['tc filter del dev eth0 parent 10: prio ', tfilter[9], ' handle ', handle_r]
+        cmd = ['tc filter del dev br100 parent 10: prio ', tfilter[9], ' handle ', handle_r]
         cmd = map(str, cmd)
         os.system(''.join(cmd))
 #        self._execute(cmd)
